48bd5080a5fe7abf144a2218f3564ceacc5f3fbe
黄欢 <huanghuan.207791@sieyuan.com>
2024-03-05 15:31:44 +0800

    update(custom): 配置功能项

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,12 +2,16 @@
 require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
+  env: {
+    node: true
+  },
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
+    'plugin:prettier/recommended'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
